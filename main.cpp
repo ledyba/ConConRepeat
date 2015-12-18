a24ff2c70d9402ab0a228c829ba6cd1d98a770f2
PSI <ledyba@users.osdn.me>
2015-12-18 16:44:54 +0900

    Signed-off-by: PSI <ledyba@users.osdn.me>

--- main.cpp
+++ main.cpp
@@ -2,9 +2,25 @@
 #include <opencv2/opencv.hpp>
 #include <vector>
 
+using namespace std;
+using namespace cv;
+double calcDiff(Mat a, Mat b){
+	double d = 0;
+	auto aptr = a.begin<Vec3b>();
+	auto bptr = b.begin<Vec3b>();
+	auto aend = a.end<Vec3b>();
+	for(;aptr != aend; ++aptr, ++bptr){
+		for(int i=0;i<3;++i){
+			double val = abs((*aptr)[i]-(*bptr)[i]);
+			if(val > 25.6){
+				d += 1;
+			}
+		}
+	}
+	return d/(a.rows * a.cols * a.channels());
+}
+
 int main(int argc, char** argv) {
-	using namespace std;
-	using namespace cv;
 	VideoCapture cap;
 	if(argc <= 1){
 		cerr << "usage: ConConRepeat <fname> [dir]" << endl;
@@ -18,32 +34,30 @@ int main(int argc, char** argv) {
 	if(argc >= 3){
 		dir = argv[2];
 	}
-	int skip=0;
+	double skip=0;
 	if(argc >= 4){
-		skip = atoi(argv[3]);
+		skip = atof(argv[3])*1000.0;
 	}
 	cout << "file: " << argv[1] << endl;
 	cout << "out : " << dir << endl;
-	cout << "skip : " << skip << endl;
+	cout << "skip : " << (skip/1000) <<" sec"<< endl;
 	double const fps = cap.get(CV_CAP_PROP_FPS);
 	int const width = 640;
 	int const height = 360;
-	vector<Mat> frames(150);
+	vector<Mat> frames((int)(fps*3));
 	int i = 0;
 	namedWindow("frame",1);
 	int found = 0;
 	Mat frame;
+	cap.set(CV_CAP_PROP_POS_MSEC, skip);
 	while(cap.read(frame)){
 		int const current = (int)cap.get(CV_CAP_PROP_POS_FRAMES);
 		Mat& now = frames[i];
 		cv::resize(frame, now, cv::Size(width,height),cv::INTER_CUBIC);
 		imshow("frame", now);
 		if(waitKey(30) >= 0) break;
-		if(skip > 0){
-			skip--;
-			continue;
-		}
 		int near = 0;
+		int far = 0;
 		bool sameFrame = false;
 		double maxd = 0;
 		double mind = DBL_MAX;
@@ -52,16 +66,16 @@ int main(int argc, char** argv) {
 			if(target.size().width != now.size().width && target.size().height != now.size().height){
 				break;
 			}
-			double const diff = norm(now,target,NORM_L2) / (width*height*3);
-			maxd = std::max(maxd, diff);
-			mind = std::min(mind, diff);
-			if(diff < 0.01){
+			double const dinf = calcDiff(target, now);
+			double const dl2 = norm(target, now, NORM_L2) / (width*height*3);
+			if(dinf <= mind) {
 				near = j;
-				sameFrame = true;
 			}
+			maxd = std::max(maxd, dl2);
+			mind = std::min(mind, dinf);
 		}
-		if(sameFrame && maxd > 0.13){
-			cout << "Found(" << found << "): " <<  (current - near) << " -> " << (current) << endl;
+		if(mind < 0.01 && maxd > 0.10){
+			cout << "Found(" << found << "): " <<  (current - near) << " -> " << (current) << ", "<< ((double)near/fps) <<"sec, range=("<<mind<<", "<<maxd<<")"<< endl;
 			for(int j=near, f=0;j>=0;--j,++f) {
 				std::string fname = dir+cv::format("/anime-%03d-%03d.png",found,f);
 				Mat& frame = frames[((i-j) + frames.size()) % frames.size()];
